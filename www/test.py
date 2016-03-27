5dece3147ba7a8663fe280784240ee218bcc0b69
chen <sadscv@hotmail.com>
2016-03-28 00:34:00 +0800

    IME error

--- www/test.py
+++ www/test.py
@@ -1,7 +1,7 @@
 import orm
 from models import User, Blog, Comment
 
-def test()£º
+def test():
 	yield from orm.create_pool(user = 'root', password = 'sadsad', database = 'awesome')
 	
 	u = User(name = 'Test', email = 'test@example.com', passwd = '123456', image = 'about:blank')
