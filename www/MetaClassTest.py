09072c5e9920dee6b57fdd71aa142c13319d567d
qiyue <sadscv@hotmail.com>
2016-07-24 21:50:59 +0800

    refactor the project.

--- www/MetaClassTest.py
+++
@@ -1,5 +0,0 @@
-from hello import Hello
-h = Hello()
-h.hello()
-print(type(Hello))
-print(type(h))
--- www/test.py
+++ www/test.py
@@ -1,18 +1,30 @@
-import orm
 import asyncio
-from models import User, Blog, Comment
+import logging
 
-def test(loop):
-	yield from orm.create_pool(loop = loop, user = 'root', password = 'sadsad', db = 'awesome')
-	
-	u = User(name = 'Test', email = 'qiyue6', passwd = '123456', image = 'about:blank')
-	
-	yield from u.save()
-	
-	
+from app.frame.orm import create_pool
+from app.models import User
+from www.config.orm_test import db_config
 
+async def test():
+    await create_pool(loop, **db_config)
 
-	
-loop = asyncio.get_event_loop()
-loop.run_until_complete(test(loop))
-# loop.run_forever()
+    rows = await User.countRows()
+    logging.info('rows is :%s' % rows)
+
+    if rows < 2:
+        for idx in range(5):
+            u = User(
+                name='test%s' % (idx),
+                email='test%s@orm.org' % (idx),
+                password='pw',
+                image='/static/img/user.png'
+            )
+            rows = await User.countRows(where='email =?', args=[u.email])
+            if rows == 0:
+                await u.save()
+            else:
+                print('the email was already used')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test())
